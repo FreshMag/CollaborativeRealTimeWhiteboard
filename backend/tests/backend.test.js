20ddebf0caff791a3b4c5976e632fcf0ad7f72a6
Valerio Di Zio <valerio.dizio@studio.unibo.it>
2023-10-01 15:07:36 +0200

    test register

--- backend/tests/backend.test.js
+++ backend/tests/backend.test.js
@@ -3,22 +3,26 @@ const request = require("supertest");
 const app = require("../server");
 const mongoose = require("mongoose");
 require("dotenv").config();
-beforeEach(async () => {
-    await mongoose.connect(`mongodb://${process.env['DB_IP']}:27017/whiteboard-db`);
+
+USER_ID = "";
+
+beforeAll(async () => {
+    await mongoose.connect(process.env.DB_ADDRESS);
 });
 
 /* Closing database connection after each test. */
-afterEach(async () => {
+afterAll(async () => {
     await mongoose.connection.close();
 });
 
 describe("POST /api/register", () => {
     it("Test User Creation", async () => {
-        request(app).post("/auth/register").send({username: "user3123@unibo.it", password: "password", first_name:"Mario", last_name:"Rossi"}).then((res) => {
-            console.error(res.body);
+        await request(app).post("/auth/register").send({username: "user@test.it", password: "password", first_name:"Mario", last_name:"Rossi"}).then((res) => {
             expect(res.statusCode).toBe(200);
-            expect(res.body.username).toBe("user3123@unibo.it");
+            USER_ID = res.body.user.id;
+            expect(res.body.user.username).toBe("user@test.it");
         });
     });
 });
+
 
