495530e99fc46609eb6c77057b9223b845d80b8c
FreshMag <pescomagnani@gmail.com>
2023-10-03 09:47:56 +0200

    Finished whiteboard creation test. Started whiteboard update test. Fixed one check on request body inside profileController.js

--- backend/tests/profile.test.js
+++ backend/tests/profile.test.js
@@ -2,10 +2,12 @@ const request = require("supertest");
 const mongoose = require("mongoose");
 const app = require("../server");
 
-const {User} = require("../src/models/dbModel");
+const {User, Whiteboard} = require("../src/models/dbModel");
 
 let USER_ID = "";
 let ACCESS_TOKEN = "";
+let CREATED_WHITEBOARDS = []
+
 beforeAll(async () => {
   await mongoose.connect(process.env.DB_ADDRESS);
   await request(app).post("/api/auth/register").send({username: "user@test.it", password: "password", first_name:"Mario", last_name:"Rossi"}).then((res) => {
@@ -22,19 +24,73 @@ beforeAll(async () => {
 
 /* Closing database connection after each test. */
 afterAll(async () => {
-  await User.findByIdAndDelete(USER_ID).then(async () => {
-    await mongoose.connection.close().then(() => {
-    });
-  });
+  await User.findByIdAndDelete(USER_ID)
+  for (let whiteboard in CREATED_WHITEBOARDS) {
+    await Whiteboard.findByIdAndDelete(whiteboard._id)
+  }
+  await mongoose.connection.close()
+
+
 });
 describe("GET /api/profile/", () => {
-  it("Test User Get Profile", async () => {
-    await request(app).get("/api/profile/")
+  it("Test User Get Profile and Whiteboard creation", async () => {
+    const res = await request(app).get("/api/profile/")
         .send()
         .set({ Authorization: `Bearer ${ACCESS_TOKEN}` })
-        .then((res) => {
-          expect(res.statusCode).toBe(200);
-          expect(res.body.whiteboards.length).toBe(0)
-    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.whiteboards.length).toBe(0)
+    const createResponse = await request(app).post("/api/profile/createWhiteboard")
+        .send({whiteboardName: "Test"})
+        .set({ Authorization: `Bearer ${ACCESS_TOKEN}` })
+    expect(createResponse.statusCode).toBe(200);
+    expect(createResponse.body).toHaveProperty("message")
+    expect(createResponse.body).toHaveProperty("whiteboardId")
+
+    CREATED_WHITEBOARDS.push(createResponse.body.whiteboardId)
+
+    const newProfileResponse = await request(app).get("/api/profile/")
+        .send()
+        .set({ Authorization: `Bearer ${ACCESS_TOKEN}` })
+    expect(newProfileResponse.statusCode).toBe(200);
+    expect(newProfileResponse.body.whiteboards.length).toBe(1)
+    const whiteboard = newProfileResponse.body.whiteboards[0];
+    expect(whiteboard).toHaveProperty("_id")
+    expect(whiteboard).toHaveProperty("ownerId")
+    expect(whiteboard).toHaveProperty("users")
+    expect(whiteboard.users.length).toBe(1)
+    expect(whiteboard.users[0]).toBe(USER_ID)
+    expect(whiteboard).toHaveProperty("name")
+    expect(whiteboard.name).toBe("Test")
   });
 });
+
+describe("GET /api/profile/updateWhiteboard", () => {
+  it("Test Update of whiteboard", async () => {
+    const res = await request(app).get("/api/profile/")
+        .send()
+        .set({ Authorization: `Bearer ${ACCESS_TOKEN}` })
+    expect(res.statusCode).toBe(200);
+    expect(res.body.whiteboards.length).toBe(1)
+    const id = res.body.whiteboards[0]._id
+    const updateResponse = await request(app).put("/api/profile/updateWhiteboard")
+        .send({whiteboardId: id, newName: "New Test"})
+        .set({ Authorization: `Bearer ${ACCESS_TOKEN}` })
+    expect(updateResponse.statusCode).toBe(200);
+    expect(updateResponse.body).toHaveProperty("message")
+
+    const newProfileResponse = await request(app).get("/api/profile/")
+        .send()
+        .set({ Authorization: `Bearer ${ACCESS_TOKEN}` })
+    expect(newProfileResponse.statusCode).toBe(200);
+    expect(newProfileResponse.body.whiteboards.length).toBe(1)
+    const whiteboard = newProfileResponse.body.whiteboards[0];
+    expect(whiteboard).toHaveProperty("_id")
+    expect(whiteboard).toHaveProperty("ownerId")
+    expect(whiteboard).toHaveProperty("users")
+    expect(whiteboard.users.length).toBe(1)
+    expect(whiteboard.users[0]).toBe(USER_ID)
+    expect(whiteboard).toHaveProperty("name")
+    expect(whiteboard.name).toBe("New Test")
+  });
+});
+
