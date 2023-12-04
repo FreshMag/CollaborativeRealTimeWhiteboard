c72ae7caf65594967f99e54c6e1840691a4cdd34
FreshMag <pescomagnani@gmail.com>
2023-12-04 22:32:41 +0100

    Started working at new auth

---
+++ auth/handlers.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func registerHandler() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+	}
+}
+
+func loginHandler() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+	}
+}
---
+++ auth/user.go
@@ -0,0 +1,8 @@
+package main
+
+type User struct {
+	username  string
+	password  string
+	firstName string
+	lastName  string
+}
